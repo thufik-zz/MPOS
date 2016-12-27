6722515c9dd34cbd0fc44dd492512a0b62cd1441
Fellipe Thufik <fellipe_thufik@hotmail.com>
2016-12-27 11:45:15 -0200

    First Commit

---
+++ MPOS/MPOS-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  MPOS-Bridging-Header.h
+//  MPOS
+//
+//  Created by Fellipe Thufik Costa Gomes Hoashi on 27/12/16.
+//  Copyright © 2016 Fellipe Thufik Costa Gomes Hoashi. All rights reserved.
+//
+
+
+#ifndef Pagar_me_Bridging_Header_h
+#define Pagar_me_Bridging_Header_h
+
+#import <mpos-ios.h>
+
+
+#endif /* Pagar_me_Bridging_Header_h */
